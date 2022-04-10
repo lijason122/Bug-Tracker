3da8f30ad049598adf20be8b7c11a22c4c2c67ef
Jason Li <lijason@sheridancollege.ca>
2022-04-10 12:11:03 -0400

    Add Sign Up functionality and routing setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import Signup from "./components/Signup"
 import { Container } from "react-bootstrap"
+import { AuthProvider } from "./contexts/AuthContext"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Dashboard from "./components/Dashboard"
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center"
-    style={{ minHeight: "100vh" }}>
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Signup />
-      </div>
-      
-    </Container>
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/signup" element={<Signup />} />
+              </Routes>
+            </AuthProvider>
+          </Router>
+        </div>
+        
+      </Container>
+    </AuthProvider>
   )
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,41 @@
-import React, { useRef, useState } from "react"
+import React, { useState, useRef } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
 
 const Signup = () => {
   const emailRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
-  
+  const { signup } = useAuth()
+  const [error, setError] = useState('')
+  // Prevent the user from clicking the sign up button and creating the same account multiple times
+  const [loading, setLoading] = useState(false) 
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError('Passwords do not match')
+    }
+
+    try {
+      setError('')
+      setLoading(true)
+      await signup(emailRef.current.value, passwordRef.current.value)
+    } catch {
+      setError('Failed to create an account')
+    }
+
+    setLoading(false)
+  }
+
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
-          <Form>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
@@ -24,7 +48,7 @@ const Signup = () => {
               <Form.Label>Password Confirmation</Form.Label>
               <Form.Control type="password" ref={passwordConfirmRef} required />
             </Form.Group>
-            <Button className="w-100" type="submit">
+            <Button disabled={loading} className="w-100" type="submit">
               Sign Up
             </Button>
           </Form>
