3f312df1d275b149f1677ca03188efeacf24f834
Jason Li <lijason@sheridancollege.ca>
2022-04-10 20:47:23 -0400

    Add Private Routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import Signup from "./components/Signup"
-import { Container } from "react-bootstrap"
-import { AuthProvider } from "./contexts/AuthContext"
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Dashboard from "./components/Dashboard"
-import Login from './components/Login'
+import Signup from "./components/Signup";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <AuthProvider>
-      <Container className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
         <div className="w-100" style={{ maxWidth: "400px" }}>
           <Router>
             <AuthProvider>
               <Routes>
-                <Route path="/" element={<Dashboard />} />
+                <Route
+                  path="/"
+                  element={
+                    <PrivateRoute>
+                      <Dashboard />
+                    </PrivateRoute>
+                  }
+                />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
               </Routes>
             </AuthProvider>
           </Router>
         </div>
-        
       </Container>
     </AuthProvider>
-  )
+  );
 }
 
 export default App;
